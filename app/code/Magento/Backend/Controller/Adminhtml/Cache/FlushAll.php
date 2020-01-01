9f83bae0d820db450eaade2a4f9c3f0ae144de96
Julian van den Berkmortel <julian@weprovide.com>
2020-01-01 20:47:29 +0100

    Refactor the "Flush Cache Storage" controller to use the cache manager

--- app/code/Magento/Backend/Controller/Adminhtml/Cache/FlushAll.php
+++ app/code/Magento/Backend/Controller/Adminhtml/Cache/FlushAll.php
@@ -25,10 +25,7 @@ class FlushAll extends \Magento\Backend\Controller\Adminhtml\Cache implements Ht
     public function execute()
     {
         $this->_eventManager->dispatch('adminhtml_cache_flush_all');
-        /** @var $cacheFrontend \Magento\Framework\Cache\FrontendInterface */
-        foreach ($this->_cacheFrontendPool as $cacheFrontend) {
-            $cacheFrontend->getBackend()->clean();
-        }
+        $this->cacheManager->flush($this->cacheManager->getAvailableTypes());
         $this->messageManager->addSuccessMessage(__("You flushed the cache storage."));
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
